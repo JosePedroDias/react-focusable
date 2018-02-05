f6d2a38e5129dd71d1b6567a37808ce0531c6f86
José Pedro Dias <jose.pedro.dias@gmail.com>
2018-02-05 00:30:38 +0000

    making it npm ready

---
+++ main.js
@@ -0,0 +1,13 @@
+import FocusableRoot from "./FocusableRoot";
+import FocusableDebugger from "./FocusableDebugger";
+import FocusableHorizontalList from "./FocusableHorizontalList";
+import FocusableVerticalList from "./FocusableVerticalList";
+import FocusableLeaf from "./FocusableLeaf";
+
+export default {
+  FocusableRoot,
+  FocusableDebugger,
+  FocusableHorizontalList,
+  FocusableVerticalList,
+  FocusableLeaf
+};
